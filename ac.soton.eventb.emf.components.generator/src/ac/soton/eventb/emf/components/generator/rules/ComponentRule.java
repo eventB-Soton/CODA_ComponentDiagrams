95f7591732f51f304c843c903d537e993e2ba5f7
Colin Snook <cfs@ecs.soton.ac.uk>
2017-03-26 14:23:53 +0100

    Copyright and licence set up as Crown copyright/M.o.D. - component diagrams

--- ac.soton.eventb.emf.components.generator/src/ac/soton/eventb/emf/components/generator/rules/ComponentRule.java
+++ ac.soton.eventb.emf.components.generator/src/ac/soton/eventb/emf/components/generator/rules/ComponentRule.java
@@ -1,16 +1,17 @@
-/**
- * (c) Crown owned copyright (2015) (UK Ministry of Defence)
- * This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0
- * International License
+/*******************************************************************************
+ *  (c) Crown owned copyright 2011, 2017 (UK Ministry of Defence)
  *  
- * This is to identify the UK Ministry of Defence as owners along with the license rights provided. The
- * URL of the CC BY NC SA 4.0 International License is 
- * http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode (Accessed 02-NOV-15).
- * 
- * Contributors:
- * 	University of Southampton - Initial implementation
+ *  All rights reserved. This program and the accompanying materials  are 
+ *  made available under the terms of the Eclipse Public License v1.0 which
+ *  accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
  *  
-*/
+ *  This is to identify the UK Ministry of Defence as owners along with the
+ *   license rights provided.
+ *  
+ *  Contributors:
+ *  			University of Southampton - Initial implementation
+ *******************************************************************************/
 package ac.soton.eventb.emf.components.generator.rules;
 
 import java.util.ArrayList;
